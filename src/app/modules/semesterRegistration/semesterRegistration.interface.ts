8d8a81ae896f9864c5f602aa22da9b81bc37eb71
NA-Asraful-Khan <soyonshikhor@gmail.com>
2024-09-28 20:42:17 +0600

    Semester Ragistration interface/model/validation complete

--- src/app/modules/semesterRegistration/semesterRegistration.interface.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.interface.ts
@@ -1,3 +1,10 @@
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
 
-export interface TSemesterRegistration extends Document {}
+export interface TSemesterRegistration extends Document {
+  academicSemester: Types.ObjectId;
+  status: 'UPCOMING' | 'ONGOING' | 'ENDED';
+  startDate: Date;
+  endDate: Date;
+  minCredit: number;
+  maxCredit: number;
+}
--- src/app/modules/semesterRegistration/semesterRegistration.model.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.model.ts
@@ -1,9 +1,50 @@
 import mongoose, { model } from 'mongoose';
 import { TSemesterRegistration } from './semesterRegistration.interface';
+import { SemesterRegistrationStatus } from './semesterRegistration.constant';
+import AppError from '../../errors/AppError';
+import httpStatus from 'http-status';
+import { AcademicSemesterModel } from '../academicSemester/academicSemester.model';
 
 const semesterRegistrationSchema = new mongoose.Schema<TSemesterRegistration>(
-  {},
+  {
+    academicSemester: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'AcademicSemester',
+      unique: true,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: SemesterRegistrationStatus,
+      default: 'UPCOMING',
+    },
+    startDate: { type: Date, required: true },
+    endDate: { type: Date, required: true },
+    minCredit: { type: Number, default: 3 },
+    maxCredit: { type: Number, default: 16 },
+  },
+  {
+    timestamps: true,
+  },
 );
+
+semesterRegistrationSchema.pre('save', async function (next) {
+  const isAcademicSemesterExist = await AcademicSemesterModel.findById(
+    this.academicSemester,
+  );
+
+  if (!isAcademicSemesterExist) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Semester Not Exist');
+  }
+  const isSemesterRegestrationExist = await SemesterRegistrationModel.findOne({
+    academicSemester: this.academicSemester,
+  });
+
+  if (isSemesterRegestrationExist) {
+    throw new AppError(httpStatus.CONFLICT, 'Semester already registered.');
+  }
+  next();
+});
 
 export const SemesterRegistrationModel = model<TSemesterRegistration>(
   'SemesterRegistration',
